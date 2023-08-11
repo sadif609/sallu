4ae2f0bc2bbbb82ee7372cd39a2f25b0ce316209
sadif609 <sadif609@gmail.com>
2023-08-11 15:35:01 +0600

    first commit

---
+++ ami.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N  INT_MAX
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<fixed<<setprecision(2);
+   cout<<"sadif";
+}
